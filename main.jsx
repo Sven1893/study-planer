a1cd79f97d656d61830322192d0b6c1b486f1578
st178702 <st178702@stud.uni-stuttgart.de>
2026-03-18 13:40:28 +0100

    Setup Project

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import StudyPlaner from './study-planer.jsx' // Make sure the export name matches your file
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <StudyPlaner />
+    </React.StrictMode>,
+)
---
+++ study-planer.jsx
@@ -0,0 +1,7 @@
+export default function StudyPlaner() {
+  return (
+    <div>
+      <h1>Study Planer</h1>
+    </div>
+  );
+}
